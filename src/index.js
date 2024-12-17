63ba86b5d7faa49c31e833532ba95f5d63e783df
Ankit Tyagi <32265726+tyagiankit1@users.noreply.github.com>
2024-12-17 10:40:37 +0530

    bug fixes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './App'
+import './index.css';
+// This is the ID of the div in your index.html file
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
